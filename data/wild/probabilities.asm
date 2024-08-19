a5c95bd5c04dcf4fb02126c66bf13b5b1a62e370
melody <melody@pallet.town>
2024-08-18 23:35:35 -0400

    Change encounter slot probabilities
    This is an attempt to reduce tedium by eliminating 1% encounter slots, which should also free up encounter slot space on routes that were doubling up on the 4% and 1% slots for the same mon, allowing realistic encounter chances for a wider variety of mons per route.
    
    TODO: Adjust actual wild routes so that this isn't completely chaotic.

--- data/wild/probabilities.asm
+++ data/wild/probabilities.asm
@@ -3,13 +3,13 @@ WildMonEncounterSlotChances:
 ; those 10 slots is. A random number is generated and then the first byte of each pair in this
 ; table is compared against that random number. If the random number is less than or equal
 ; to the first byte, then that slot is chosen.  The second byte is double the slot number.
-	db  50, $00 ; 51/256 = 19.9% chance of slot 0
-	db 101, $02 ; 51/256 = 19.9% chance of slot 1
-	db 140, $04 ; 39/256 = 15.2% chance of slot 2
-	db 165, $06 ; 25/256 =  9.8% chance of slot 3
-	db 190, $08 ; 25/256 =  9.8% chance of slot 4
-	db 215, $0A ; 25/256 =  9.8% chance of slot 5
-	db 228, $0C ; 13/256 =  5.1% chance of slot 6
-	db 241, $0E ; 13/256 =  5.1% chance of slot 7
-	db 252, $10 ; 11/256 =  4.3% chance of slot 8
-	db 255, $12 ;  3/256 =  1.2% chance of slot 9
+	db  51, $00 ; 52/256 = 20.3% chance of slot 0
+	db 103, $02 ; 52/256 = 20.3% chance of slot 1
+	db 128, $04 ; 25/256 =  9.8% chance of slot 2
+	db 153, $06 ; 25/256 =  9.8% chance of slot 3
+	db 178, $08 ; 25/256 =  9.8% chance of slot 4
+	db 203, $0A ; 25/256 =  9.8% chance of slot 5
+	db 216, $0C ; 13/256 =  5.1% chance of slot 6
+	db 229, $0E ; 13/256 =  5.1% chance of slot 7
+	db 242, $10 ; 13/256 =  5.1% chance of slot 8
+	db 255, $12 ; 13/256 =  5.1% chance of slot 9
