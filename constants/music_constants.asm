544ce2190d2bfe2b3576fc5d51061a38d93406fd
melody <melody@pallet.town>
2024-08-18 23:35:35 -0400

    fix mistaken change to SFX constant

--- constants/music_constants.asm
+++ constants/music_constants.asm
@@ -200,7 +200,7 @@ DEF BATTLE_SFX_START EQUS "SFX_PECK"
 	music_const SFX_BATTLE_1C,          SFX_Battle_1C
 	music_const SFX_DOUBLESLAP,         SFX_Doubleslap
 	music_const SFX_BATTLE_1E,          SFX_Battle_1E
-	music_const SFX_BUG_BUZZ,         SFX_Horn_Drill
+	music_const SFX_HORN_DRILL,         SFX_Horn_Drill
 	music_const SFX_BATTLE_20,          SFX_Battle_20
 	music_const SFX_BATTLE_21,          SFX_Battle_21
 	music_const SFX_BATTLE_22,          SFX_Battle_22
