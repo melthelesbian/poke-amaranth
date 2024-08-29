a0b9f00c86f63d8acf3aed1c6db5bdef542370ba
melody <melody@pallet.town>
2024-08-29 12:02:32 -0400

    gyarados learnset

--- data/pokemon/evos_moves/gyarados.asm
+++ data/pokemon/evos_moves/gyarados.asm
@@ -2,6 +2,15 @@ GyaradosEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 1, TACKLE
+	db 1, WATER_GUN
 	db 1, LEER
+	db 15, TACKLE
+	db 20, DRAGON_RAGE
+	db 25, WHIRLPOOL
+	db 30, BITE
+	db 36, WATERFALL
+	db 42, DRAGONBREATH
+	db 48, HYDRO_PUMP
+	db 55, GLARE
+	db 63, THRASH
 	db 0
