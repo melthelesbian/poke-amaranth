0f5465ca75cdf383e86026a900b8aaf7b471ebc6
melody <melody@pallet.town>
2024-09-08 17:40:17 -0400

    mew learnset

--- data/pokemon/evos_moves/mew.asm
+++ data/pokemon/evos_moves/mew.asm
@@ -2,5 +2,18 @@ MewEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db  1, POUND
+	db 1, POUND
+	db 5, CONFUSION
+	db 10, TRANSFORM
+	db 15, DRAIN_KISS
+	db 20, MEGA_PUNCH
+	db 25, PSYBEAM
+	db 30, MIMIC
+	db 35, BARRIER
+	db 40, DOUBLE_DASH
+	db 45, BODY_SLAM
+	db 50, PSYCHIC_M
+	db 55, MINIMIZE
+	db 60, MOONBLAST
+	db 65, METRONOME
 	db 0
