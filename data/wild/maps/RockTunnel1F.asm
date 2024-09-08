1cc4498199d165616c697b4274283e037e6b453b
melody <melody@pallet.town>
2024-09-08 14:02:04 -0400

    rock tunnel encounters

--- data/wild/maps/RockTunnel1F.asm
+++ data/wild/maps/RockTunnel1F.asm
@@ -1,15 +1,15 @@
 RockTunnel1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, ZUBAT
-	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
-	db 15, ZUBAT
-	db 17, MACHOP
-	db 13, ONIX
-	db 15, ONIX
+	db 21, ZUBAT      ; 20%
+	db 24, GEODUDE    ; 20%
+	db 23, GOLBAT     ; 10%
+	db 26, GRAVELER   ; 10%
+	db 25, ONIX       ; 10%
+	db 24, RHYHORN    ; 10%
+	db 28, ONIX       ;  5%
+	db 27, RHYHORN    ;  5%
+	db 20, KANGASKHAN ;  5%
+	db 25, KANGASKHAN ;  5%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/RockTunnelB1F.asm
+++ data/wild/maps/RockTunnelB1F.asm
@@ -1,15 +1,15 @@
 RockTunnelB1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, ZUBAT
-	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
-	db 17, MACHOP
-	db 17, ONIX
-	db 13, ONIX
-	db 18, GEODUDE
+	db 21, ZUBAT      ; 20%
+	db 23, GEODUDE    ; 20%
+	db 23, PARAS      ; 10%
+	db 24, ONIX       ; 10%
+	db 27, ONIX       ; 10%
+	db 24, RHYHORN    ; 10%
+	db 28, PARASECT   ;  5%
+	db 27, GRAVELER   ;  5%
+	db 20, KANGASKHAN ;  5%
+	db 25, KANGASKHAN ;  5%
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
