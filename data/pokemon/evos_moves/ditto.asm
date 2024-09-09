85bc087adb09f8cef2841c708621e97dcb5e1ab9
melody <melody@pallet.town>
2024-09-09 03:37:19 -0400

    snorlax and ditto learnsets

--- data/pokemon/evos_moves/ditto.asm
+++ data/pokemon/evos_moves/ditto.asm
@@ -2,5 +2,10 @@ DittoEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 1, TRANSFORM
+	db  1, TRANSFORM
+	db 10, MIRROR_MOVE
+	db 20, MINIMIZE
+	db 30, MIMIC
+	db 40, SUBSTITUTE
+	db 50, METRONOME
 	db 0
--- data/pokemon/evos_moves/snorlax.asm
+++ data/pokemon/evos_moves/snorlax.asm
@@ -3,5 +3,13 @@ SnorlaxEvosMoves:
 	db 0
 ; Learnset
 	db 1, TACKLE
-	db 1, REST
+	db 1, GROWL
+	db 9, AMNESIA
+	db 14, DEFENSE_CURL
+	db 20, HEADBUTT
+	db 26, REST
+	db 33, HEAVY_SMASH
+	db 40, PREPARE
+	db 48, TAKE_DOWN
+	db 57, HYPER_BEAM
 	db 0
