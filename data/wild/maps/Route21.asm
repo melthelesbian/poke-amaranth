c3ef533639267dbf6982e7465b8ba1ea529e1e50
melody <melody@pallet.town>
2024-09-18 20:22:32 -0400

    route 21 wild encounters

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -1,26 +1,26 @@
 Route21WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 21, RATTATA
-	db 23, PIDGEY
-	db 30, RATICATE
-	db 23, RATTATA
-	db 21, PIDGEY
-	db 30, PIDGEOTTO
-	db 32, PIDGEOTTO
-	db 28, TANGELA
-	db 30, TANGELA
-	db 32, TANGELA
+	db 32, EEVEE      ; 20%
+	db 33, PIKACHU    ; 20%
+	db 35, IVYSAUR    ; 10%
+	db 35, CHARMELEON ; 10%
+	db 35, WARTORTLE  ; 10%
+	db 34, FARFETCHD  ; 10%
+	db 31, BULBASAUR  ; 5%
+	db 31, CHARMANDER ; 5%
+	db 31, SQUIRTLE   ; 5%
+	db 35, FARFETCHD  ; 5%
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACOOL
-	db 40, TENTACOOL
+	db 35, TENTACOOL  ; 20%
+	db 34, SEAKING    ; 20%
+	db 33, TENTACOOL  ; 10%
+	db 36, GOLDEEN    ; 10%
+	db 35, MAGIKARP   ; 10%
+	db 33, MAGIKARP   ; 10%
+	db 35, TENTACRUEL ; 5%
+	db 34, GYARADOS   ; 5%
+	db 32, LAPRAS     ; 5%
+	db 35, LAPRAS     ; 5%
 	end_water_wildmons
