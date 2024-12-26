ca184271903cd7d4ca9410ab216acfa7f2f53f01
melody <melody@pallet.town>
2024-12-25 23:34:53 -0500

    preliminary mewtwo learnset

--- data/pokemon/evos_moves/mewtwo.asm
+++ data/pokemon/evos_moves/mewtwo.asm
@@ -4,4 +4,13 @@ MewtwoEvosMoves:
 ; Learnset
 	db 1, CONFUSION
 	db 1, LEER
+	db 8, DISABLE
+	db 15, MEGA_PUNCH
+	db 22, PSYBEAM
+	db 30, BARRIER
+	db 38, FEINT_ATTACK
+	db 45, RECOVER
+	db 54, PSYCHIC_M
+	db 64, AMNESIA
+	db 75, SHADOW_TEAR
 	db 0
