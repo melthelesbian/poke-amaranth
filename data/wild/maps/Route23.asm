ad084cc08cbba3b4f37b428f8edf94bc3f24dc69
melody <melody@pallet.town>
2024-12-28 18:19:07 -0500

    relevel route 23 encounters to fix curve

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,26 +1,26 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 43, ARBOK     ; 20%
-	db 44, SANDSLASH ; 20%
-	db 45, PIDGEOT   ; 10%
-	db 46, EXEGGUTOR ; 10%
-	db 44, PRIMEAPE  ; 10%
-	db 45, FEAROW    ; 10%
-	db 44, PIDGEOT   ; 5%
-	db 43, EXEGGUTOR ; 5%
-	db 45, CHANSEY   ; 5%
+	db 48, ARBOK     ; 20%
+	db 49, SANDSLASH ; 20%
+	db 50, PIDGEOT   ; 10%
+	db 51, EXEGGUTOR ; 10%
+	db 49, PRIMEAPE  ; 10%
+	db 50, FEAROW    ; 10%
+	db 49, PIDGEOT   ; 5%
+	db 48, EXEGGUTOR ; 5%
 	db 50, CHANSEY   ; 5%
+	db 55, CHANSEY   ; 5%
 	end_grass_wildmons
 
 	def_water_wildmons 10 ; encounter rate
-	db 44, GOLDUCK   ; 20%
-	db 45, SLOWBRO   ; 20%
-	db 45, SEAKING   ; 10%
-	db 46, KINGLER   ; 10%
-	db 46, POLIWRATH ; 10%
-	db 45, LAPRAS    ; 10%
-	db 46, SEAKING   ; 5%
-	db 47, KINGLER   ; 5%
-	db 45, VAPOREON  ; 5%
+	db 49, GOLDUCK   ; 20%
+	db 50, SLOWBRO   ; 20%
+	db 50, SEAKING   ; 10%
+	db 51, KINGLER   ; 10%
+	db 51, POLIWRATH ; 10%
+	db 51, LAPRAS    ; 10%
+	db 51, SEAKING   ; 5%
+	db 52, KINGLER   ; 5%
 	db 50, VAPOREON  ; 5%
+	db 55, VAPOREON  ; 5%
 	end_water_wildmons
