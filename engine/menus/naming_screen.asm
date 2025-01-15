de58417d2f59807f2386c7f5cc301bb9fd95b248
melody <melody@pallet.town>
2025-01-15 00:53:38 -0500

    fix naming screen bug

--- engine/menus/naming_screen.asm
+++ engine/menus/naming_screen.asm
@@ -468,7 +468,6 @@ PrintNamingText:
 	call PlaceString
 	ld hl, $1
 	add hl, bc
-	ld [hl], "の" ; leftover from Japanese version; blank tile $c9 in English
 	hlcoord 1, 3
 	ld de, NicknameTextString
 	jr .placeString
