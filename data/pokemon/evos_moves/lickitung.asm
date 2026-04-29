653bfa3fef1f4eead5d848ee609605cfb12d9fe5
melody <melody@pallet.town>
2026-04-29 18:19:13 -0400

    tweak lickitung learnset

--- data/pokemon/evos_moves/lickitung.asm
+++ data/pokemon/evos_moves/lickitung.asm
@@ -6,11 +6,11 @@ LickitungEvosMoves:
 	db 1, GROWL
 	db 6, BUBBLE
 	db 10, DEFENSE_CURL
-	db 14, LEER
-	db 15, ACID
-	db 20, CONFUSE_RAY
-	db 26, HEADBUTT
-	db 32, FRIGHTEN
+	db 14, ACID
+	db 18, CONFUSE_RAY
+	db 22, WRAP
+	db 26, GLARE
+	db 32, HEADBUTT
 	db 38, HYPNOSIS
 	db 45, LEECH_LIFE
 	db 52, TAKE_DOWN
